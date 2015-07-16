22ae724e021e26db3443ae941f0fa22e03a55148
Ryan Ciecalone <ryan@ryanchristianmedia.com>
2015-07-15 21:39:47 -0600

    Added code to 99 bottles of beer .js and .html files

---
+++ script.js
@@ -0,0 +1,15 @@
+var beerSong = function(){
+for (var bottlesAfter = 99;bottlesAfter >= 0; bottlesAfter -= 1){
+    var numBottles = bottlesAfter --;
+    if (numBottles === 1){
+      console.log(numBottles + " Bottle of beer on the wall, "
+        + numBottles + " bottle of beer. Take one down, pass it around, no more bottles of beer on the wall!")
+    }
+    else{
+  console.log(numBottles + " Bottles of beer on the wall, "
+    + numBottles + " bottles of beer. Take one down, pass it around, " + bottlesAfter + " bottles of beer on the wall!")
+    }
+  }
+}
+
+console.log(beerSong())
